3f00a8c0c67ae663a990a7226a74247e941bae97
Fernando <fernando973@gmail.com>
2020-06-10 13:23:43 -0300

    Start commit!

--- app/src/main/java/cursoandroid/example/com/organizze/MainActivity.java
+++ app/src/main/java/cursoandroid/example/com/organizze/MainActivity.java
@@ -4,11 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        //Exibe os sliders
+        ShowSlider();
+
     }
+
+    protected void ShowSlider(){
+
+        //Desativar botões
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        //Chamada dos slides
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .canGoForward(false) // não permite avançar pra próximo slide
+                .build());
+
+
+
+    }
+
+
 }
+
+
